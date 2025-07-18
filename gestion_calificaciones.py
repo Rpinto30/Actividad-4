71917ad074235758a04c5ea821fdc3c767b3e74f
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-18 09:16:08 -0600

    Creacion de bucle del menu principal

--- gestion_calificaciones.py
+++ gestion_calificaciones.py
@@ -1,1 +1,16 @@
-print("Hola programa")
+print("-"*20+"Bienvenido"+"-"*20)
+
+while True:
+    print("-"*10+"Opciones"+"-"*10)
+    print("  1) Agregar Estudiante\n  2) Mostrar Lista de Estudiantes\n  3) Calcular promedio general\n  4) Salir")
+    option = int(input(">Selecciona una opcion: "))
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option == 4:
+        pass
+    else:
+        print("Entrada no valida")

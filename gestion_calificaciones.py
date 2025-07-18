113d5071ddb1a35e40e8f8648a7fc5cb3ff5fc98
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-18 09:21:44 -0600

    Creacion de la opcion de agreagar a un estudiante

--- gestion_calificaciones.py
+++ gestion_calificaciones.py
@@ -1,11 +1,18 @@
 print("-"*20+"Bienvenido"+"-"*20)
 
+regist_students = {}
+
 while True:
     print("-"*10+"Opciones"+"-"*10)
     print("  1) Agregar Estudiante\n  2) Mostrar Lista de Estudiantes\n  3) Calcular promedio general\n  4) Salir")
     option = int(input(">Selecciona una opcion: "))
     if option == 1:
-        pass
+        print("\n"+"-"*20+"Agrega a un estudiante"+"-"*20)
+        name = input("Ingresa el nombre del nuevo estudiante:")
+        points = int(input("Ingresa la nota del nuevo estudiante: "))
+        #FUNCION SETDEFAULT PARA AGREGAR UN UNICO NOMBRE
+            #Sacado de: https://www.w3schools.com/python/ref_dictionary_setdefault.asp
+        regist_students.setdefault(name, points)
     elif option == 2:
         pass
     elif option == 3:

1e839f5e956102d7902362aabdb670178b7f4a25
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-20 21:13:08 -0600

    Creacion en el ejercicio 10 funcion que permite calcular los días entre varios meses pero siempre en el mismo año

--- 10_comparador_fechas.py
+++ 10_comparador_fechas.py
@@ -1,7 +1,7 @@
 #dates = [] #[20,7,2025]
 
-dates = [[21,7,2024],[30,8,2025]]
-#dates = [[19,7,2025],[30,6,2025]]
+#dates = [[21,7,2024],[30,8,2025]]
+dates = [[10,1,2024],[3,11,2024]]
 
 #Detección de días segun el mes
 def is_leap(year_leap): return (year_leap % 4 == 0 and year_leap % 100 != 0) or (year_leap % 400 == 0)
@@ -13,7 +13,15 @@ def month_true(month_day, year_day):
     elif month_day in [1, 3, 5, 7, 8, 10, 12]: return 31 #MESES CON 31 DÍAS
     else: return 0 #entrada no valida
 
-def days_on_month(date): return month_true(date[1], date[2]) - date[0]
+def days_on_month(date): return month_true(date[1], date[2]) - date[0] +1
+def days_between_months(d_minus,d_mayor):
+    total = 0
+    for i in range(d_minus[1]+1, d_mayor[1]):
+        print(i)
+        total += month_true(i,d_mayor[2])
+    print(days_on_month(d_minus), total)
+    total += days_on_month(d_minus) + d_mayor[0]
+    return total
 
 #Solo imprimen mensajes
 def mayor_date(date_1, date_2): print(f"La fecha {date_1[0]}/{date_1[1]}/{date_1[2]} es mayor a la fecha {date_2[0]}/{date_2[1]}/{date_2[2]}")
@@ -43,15 +51,14 @@ if len(dates) > 0:
         equals_date(dates[0], dates[1], "año")
         if dates[0][1] > dates[1][1]: #FECHA 1 MAYOR QUE FECHA 2
             mayor_date(dates[0], dates[1])
-            #days_between = abs(month_true(dates[1][1], dates[1][2])-dates[1][0]+1) + dates[0][0]
+            days_between += days_between_months(dates[1], dates[0])
         elif dates[0][1] < dates[1][1]: #FECHA 2 MAYOR QUE FECHA 1
             mayor_date(dates[1], dates[0])
-            #days_between = abs(month_true(dates[0][1], dates[0][2]) -dates[0][0]+1) + dates[1][0]
+            days_between += days_between_months(dates[0], dates[1])
         else: #MESES IGUALES
             if dates[0][0] == dates[1][0]: print("Ambas fechas son iguales")
             else:
-                days_between += abs(dates[0][0] - dates[1][0])
+                days_between += abs(dates[0][0] - dates[1][0]) +1
                 equals_date(dates[0], dates[1], "mes y año")
 
-    print(days_on_month([20,7,2025]))
     print(f"\nLos días que han pasado entre ambas fechas son: {days_between}")

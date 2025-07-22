9c646d2d7f10dcb4bfaee25623dfc15866ea279a
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-21 19:06:12 -0600

    Corección de error en ejercicio 7, ahora sí se añade la curva si y solo si todos los estudiantes tienen un promedio menor a 70

--- 7_calificaciones_curva.py
+++ 7_calificaciones_curva.py
@@ -1,22 +1,24 @@
 students = {}
 
-for i in range(5):
+for i in range(2):
     print('\n'+"-"*20+f"Estudiante {i+1}"+"-"*20)
     list_notes = []
     name = input(f"Ingrese el nombre del estudiante {i+1}: ")
     for j in range(3):
         note = int(input(f"  Ingrese la nota {j+1} del estudiante {name}: "))
         list_notes.append(note)
-    prom = round(sum(list_notes)/3, 2)
-    if prom < 70:
-        for k in range(len(list_notes)):
-            if list_notes[k] + 5 <= 100: list_notes[k] += 5
-            else: list_notes[k] = 100
-        prom = round(sum(list_notes) / 3, 2)
-
     list_notes.append(round(sum(list_notes)/3, 2))
-    #APLICACION DE CURVA
     students[name] = list_notes
+for i in students:
+    if all(students[i][-1] < 70 for e in students[i]):
+        for k in range(len(students[i])):
+            if students[i][k] + 5 <= 100: students[i][k] += 5
+            else: students[i][k] = 100
+        prom = round(sum(students[i]) / 3, 2)
+
+    students[i].append(round(sum(students[i])/3, 2))
+    #APLICACION DE CURVA
+
 
 print('\n'+f"{'Nombre':<10}{'Promedio'}")
 for i in students:print(f"{i:<10}{students[i][3]}")
